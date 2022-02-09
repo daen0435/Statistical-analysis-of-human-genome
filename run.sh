42c7aad8084717b8fd9dee365a96e551ea15bc97
daen0435 <95375330+daen0435@users.noreply.github.com>
2022-02-09 15:30:29 -0700

    Create run.sh
    updated run.sh from 05_NF_core to reference directory containing .fai, updated nf core chipseq from 1.2.1 to 1.2.2, and changed paired end selection to single end

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -p short
+#SBATCH --job-name=PolII_test
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=daen0435@colorado.edu 
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=6gb
+#SBATCH --time=14:00:00
+#SBATCH --output=nextflow3.out
+#SBATCH --error=nextflow3.err
+pwd; hostname; date
+echo "Lets go"
+module load singularity/3.1.1
+nextflow run nf-core/chipseq -r 1.2.2 \
+-profile singularity \
+--single_end \
+--input design.csv \
+--fasta /scratch/Shares/rinnclass/CLASS_2022/data/genomes/GRCh38.p13.genome.fa \
+--gtf /scratch/Shares/rinnclass/CLASS_2022/data/gencode.v32.annotation.gtf \
+--macs_gsize 3.2e9 \
+--blacklist /scratch/Shares/rinnclass/CLASS_2022/data/hg38-blacklist.v2.bed \
+--email daen0435@colorado.edu \
+-resume \
+-c nextflow.config
+date
